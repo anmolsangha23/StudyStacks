429b7e02a87c172efba86a68671ae09a50fd4a4a
Anmol Sangha <anmolsangha0@gmail.com>
2022-02-07 18:17:30 -0800

    created StudyStacksApp UI class

---
+++ src/main/ui/StudyStacksApp.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.util.Scanner;
+import model.*;
+
+public class StudyStacksApp {
+    private CardStack stack;
+    private Scanner input;
+
+    // EFFECTS: runs the StudyStacks application
+    public StudyStacksApp() {
+        runStudyStacks();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes service
+    private void runStudyStacks() {
+        boolean active = true;
+
+        while (active) {
+            // run commands
+        }
+    }
+
+}
